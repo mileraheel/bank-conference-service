64419cb28de195dc40c522ee61bb4792160990ce
Raheel Khan <mileraheel@gmail.com>
2024-01-19 10:34:50 +0400

    Update MeetingRequestDto.java
    adding more validators

--- src/main/java/com/bank/meetingservice/dto/request/MeetingRequestDto.java
+++ src/main/java/com/bank/meetingservice/dto/request/MeetingRequestDto.java
@@ -1,7 +1,9 @@
 package com.bank.meetingservice.dto.request;
 
 import com.bank.meetingservice.entity.Room;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -12,10 +14,12 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class MeetingRequestDto {
 
-    @NotNull(message = "meeting title cannot be blank")
+    @NotBlank(message = "meeting title cannot be empty or null")
+    @Pattern(regexp = "^[^<>{}\"/|;:.,~!?@#$%^=&*\\]\\\\()\\[¿§«»ω⊙¤°℃℉€¥£¢¡®©_+]*$",
+            message = "please do not enter any special characters in meeting title")
     private String title;
 
-    @NotNull
+    @NotNull(message = "please provide number of people for the meeting")
     private Integer people;
 
     private Room room;
